67419257fb9c3e907bef357c4b03f073b3a90462
Andrea Lottarini <lottarini@cs.columbia.edu>
2018-08-04 20:47:12 -0400

    much stuff - utilization and area lost for dbmesh

--- gather.py
+++ gather.py
@@ -19,10 +19,13 @@ if library == "synopsys":
 elif library == "tsmc":
     dbmesh_core_area = 36897.120848 + 12111.1203
     freq = 950
-    router_static_pwr  = 0.000125
-    router_dynamic_pwr = 0.000308
+    #old
+    # router_static_pwr  = 0.000125
+    # router_dynamic_pwr = 0.000308
     dbmesh_static_pwr  = 0.000611
     dbmesh_dynamic_pwr = 0.009015
+    router_static_pwr  = 0.0001433
+    router_dynamic_pwr = 0.00813    
 else:
     raise NotImplementedError
 
@@ -31,21 +34,26 @@ working_queries = [1,2,3,4,5,6,7,8,9,10,11,12,14,16,17,18,19,20,22]
 def get_latencies(d):
     latency = []
     noc_cycles = []
-    compute_cycles = []
-    for i in range(1,23):
+    compute_cycles = [] # this consider each PE working for the entire directive
+    tile_cycles = [] # this is more precise and considers load inbalance
+    for i in working_queries:
         stat_file = os.path.join(d,str(i),"stats")
         if os.path.isfile(stat_file):
             with open(stat_file) as f:
-                ct, ccc, ncc = analyze_stats(f)
+                ct, ccc, ncc, tcc = analyze_stats(f)
                     
                 latency.append( ct )
                 noc_cycles.append(ncc)
+                tile_cycles.append(tcc)
                 compute_cycles.append(ccc)
                 #print "{:<4}\t{:>10}".format(i,cc)
         else:
             latency.append(0)
+            noc_cycles.append(0)
+            compute_cycles.append(0)
+            tile_cycles.append(0)
             
-    return latency, compute_cycles, noc_cycles
+    return latency, compute_cycles, noc_cycles, tile_cycles
 
 def analyze_stats(stat_file):
     ''' Returns the completion time of the query, time spent in tiles, time spent in routers '''
@@ -53,6 +61,7 @@ def analyze_stats(stat_file):
 
     tile_utilization = 0
     noc_utilization = 0
+    tile_cycles = 0
     for stat_line in stat_file:
         if re.search("Area",stat_line):
             continue
@@ -62,19 +71,25 @@ def analyze_stats(stat_file):
             directive_latency = float(values[2])
             query_latency += directive_latency
             assert  values[2] == values[3] 
-
             try:
                 tile_utilization +=  int(values[6])  * directive_latency
                 noc_utilization  +=  ( int(values[7]) + int(values[6]) )* directive_latency # clock cycles in which the routers are active
             except:
                 noc_utilization  = 0
                 tile_utilization = 0
+        elif (values[1] == "Tile"):
+            if (len(values) >= 9):
+                # multiply latency by number of tile used
+                tile_cycles += int(values[4]) * float(values[8])
                 
-    return ( query_latency, tile_utilization, noc_utilization )
+    return ( query_latency, tile_utilization, noc_utilization, tile_cycles )
 
 if __name__ == "__main__":
 
     area_used = [[] for i in range(19)] # how much area is used for each query by the different systems
+    area_lost_unbalance = [[] for i in range(19)] # how much area is lost by co-scheduling things with different latency
+    area_lost_unused = [[] for i in range(19)] # how much area is not necessary
+    
     for d in os.listdir("."):
         if len(sys.argv) == 2:
             assert sys.argv[1] in ["REAL","DUMB"]
@@ -86,9 +101,15 @@ if __name__ == "__main__":
             width = int(m.groups()[0])
             depth = int(m.groups()[1])
             area  = width * depth * dbmesh_core_area
-            latencies, tile_cycles, router_cycles = get_latencies(d)
-            if latencies is not None:
-                latencies = [latencies[i-1] for i in working_queries] # so the geomean is not 0
+            latencies, tile_cycles, router_cycles, tile_precise_cycles = get_latencies(d)
+
+            for i in range(len(tile_precise_cycles)):
+                if latencies[i] > 0 and tile_precise_cycles[i] > 0:
+                    area_used[i].append(tile_precise_cycles[i]/(float(width*depth)*latencies[i]))
+                    assert tile_cycles[i] >= tile_precise_cycles[i]
+                    area_lost_unbalance[i].append( (tile_cycles[i]-tile_precise_cycles[i]) / (float(width*depth)*latencies[i]) )
+                    area_lost_unused[i].append( ( (float(width*depth)*latencies[i]) - tile_cycles[i]) / (float(width*depth)*latencies[i]) )
+
             if latencies is not None and 0 not in latencies:
                 cc_geomean = scipy.stats.gmean(latencies)
                 geomean = cc_geomean * float( 1.0 / float(freq * 1000) )
@@ -96,14 +117,14 @@ if __name__ == "__main__":
                 if latency == 0:
                     continue
                     #pdb.set_trace()
-                if all([ x > 0 for x in tile_cycles]): # in some cases I re-ran a few queries so not all have utilization info
+                    
+                if all([ x > 0 for x in tile_cycles ]): # in some cases I re-ran a few queries so not all have utilization info
                     router_utilization = float(sum(router_cycles)) / sum(latencies)
-                    tile_utilization   = float(sum(tile_cycles)) / sum(latencies)
+                    #tile_utilization   = float(sum(tile_precise_cycles)) / sum(latencies)
+                    tile_utilization = float(sum(tile_cycles)) / sum(latencies)
 
                     assert router_utilization < width*depth
                     assert tile_utilization < width * depth
-                    for i in range(len(tile_cycles)):
-                        area_used[i].append(tile_cycles[i]/(float(width*depth)*latencies[i]))
                     static_compute_pwr = width*depth*dbmesh_static_pwr
                     dyn_compute_pwr    = dbmesh_dynamic_pwr * tile_utilization
                     static_comm_pwr    = width*depth*router_static_pwr
@@ -124,7 +145,34 @@ if __name__ == "__main__":
 
     fig,ax = plt.subplots()
     ax.boxplot(area_used)
-    ax.set_ylabel('Average DbMesh Area Used [No load imbalance]')
+    ax.set_ylabel('Average DbMesh Area Used')
     ax.set_xlabel('Queries')
-    fig.savefig("dbmesh_used_no_inbalance.pdf")
+    ax.set_ylim([0,1])
+    fig.savefig("dbmesh_area_used.pdf")
+
+    fig1,ax1 = plt.subplots()
+    ax1.boxplot(area_lost_unbalance)
+    ax1.set_ylabel('DbMesh Area unused due to load imbalance')
+    ax1.set_xlabel('Queries')
+    ax1.set_ylim([0,1])
+    fig1.savefig("dbmesh_area_lost_unbalance.pdf")
+
+    fig2,ax2 = plt.subplots()
+    ax2.boxplot(area_lost_unused)
+    ax2.set_ylabel('DbMesh Area due to inutilization')
+    ax2.set_xlabel('Queries')
+    ax2.set_ylim([0,1])
+    fig2.savefig("dbmesh_area_lost_unused.pdf")
+
+    # DUMP THE CSV
+    csvs = {"dbmesh_area_lost_unused.csv":area_lost_unused,
+            "dbmesh_area_lost_unbalance.csv":area_lost_unbalance
+                }
+        
     
+    for k,v in csvs.items():
+        with open(k,'w') as f:
+            for query_data in v:
+                f.write(",".join([str(x) for x in query_data])+"\n")
+
+                                                   
--- maker.py
+++ maker.py
@@ -1,12 +1,22 @@
 import os
 import sys
 import errno
-
-assert os.path.isdir(sys.argv[1]) #directory where all folders are created
-
+import shutil
 '''
-This script will create a set of dirs 1-22 in folder sys.arg[1] 
+This script will create a set of dirs 1-22 in folder sys.arg[1] and create appropriate scripts for condor execution
 '''
+
+
+try:
+    assert os.path.isdir(sys.argv[1]) #directory where all folders are created
+    width = int(sys.argv[2])
+    depth = int(sys.argv[3])
+    schedule = sys.argv[4]
+    assert schedule in ["real","ideal","dumb"]
+except:
+    print >> sys.stderr , "Usage: maker.py <folder> <width> <depth> <schedule_algo>"
+    raise
+
 
 #condor_scripts = [ "script", "condor.submit"]
 condor_scripts = [ "condor.submit","cut.py"]
@@ -14,8 +24,22 @@ condor_scripts = [ "condor.submit","cut.py"]
 for e in condor_scripts:
     assert os.path.isfile(e)
     # copy the condor scripts
-    os.symlink(os.path.abspath(e), os.path.abspath(os.path.join(sys.argv[1],e)))
-                   
+    #os.symlink(os.path.abspath(e), os.path.abspath(os.path.join(sys.argv[1],e)))
+    shutil.copy(os.path.abspath(e), os.path.abspath(os.path.join(sys.argv[1],e)))
+    
+assert os.path.isfile("script")
+dest = os.path.join(sys.argv[1],"script")
+assert os.path.abspath("script") != os.path.abspath(dest)
+with open("script") as f , open(dest,'w') as g:
+    for line in f:
+
+        line = line.replace("$2",str(width))
+        line = line.replace("$3",str(depth))
+        line = line.replace("$4",schedule)
+
+        g.write(line)
+os.chmod(dest,0744)
+
 for i in xrange(1,23):
     new_dir = os.path.join(sys.argv[1],str(i))
 
